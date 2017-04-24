636f9e3f7dd6c5b2794c2574ecf3dc8deb4efb21
CharmedSatyr <email@charmedsatyr.com>
2017-04-24 09:13:32 +0900

    Completed Clementine.js tutorial for Click Counter app; it's functional

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 'use strict';
 
 const express = require('express'),
-	routes = require('./app/routes/routes.js'),
+	routes = require('./app/routes/index.js'),
 	mongo = require('mongodb').MongoClient;
 
-const port = 8080,
+const port = 3000,
 	dbport = 27017;
 
 const app = express();
 
-mongo.connect('mongodb://localhost:27017/voting_app', (err, db) => {
+mongo.connect('mongodb://localhost:' + dbport + '/voting_app', (err, db) => {
 
 	(err)
 		? console.error('Database failed to connect!')
 		: console.log('MongoDB successfully connected on port', dbport);
 
-	app.use('/views', express.static(process.cwd() + '/views'));
+	app.use('/public', express.static(process.cwd() + '/public'));
 	app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
 	routes(app, db);
